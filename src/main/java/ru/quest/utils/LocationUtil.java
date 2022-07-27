992e0d22baff28e22226f11f18dfb246af30281c
Bulatov-ar <artem-bulatov777@mail.ru>
2022-07-27 21:16:19 +0300

    Уменьшил точность при сравнении геопозиции

--- src/main/java/ru/quest/utils/LocationUtil.java
+++ src/main/java/ru/quest/utils/LocationUtil.java
@@ -9,6 +9,6 @@ public class LocationUtil {
     public static boolean compareLocations(Location location1, Location location2) {
         double x = Math.abs(location1.getLatitude() - location2.getLatitude());
         double y = Math.abs(location1.getLongitude() - location2.getLongitude());
-        return x < 0.001 && y < 0.001;
+        return x < 0.003 && y < 0.003;
     }
 }
